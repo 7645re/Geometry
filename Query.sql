80db6ad65b11fc6969cb24ceb0df53feee30937c
7645re <89273037+7645re@users.noreply.github.com>
2022-05-08 04:48:45 +0300

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -25,6 +25,6 @@ FOREIGN KEY(CategoryId)
 REFERENCES Category (Id);
 
 SELECT p.Name AS ProductName, c.Name AS CategoryName
-FROM Product p left JOIN ProductCategory pc ON pc.ProductId = p.Id
-left JOIN Category c ON pc.CategoryId = c.Id
+FROM Product p LEFT JOIN ProductCategory pc ON pc.ProductId = p.Id
+LEFT JOIN Category c ON pc.CategoryId = c.Id
 ORDER BY p.Name, c.Name
